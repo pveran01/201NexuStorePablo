f29902e12fb0cfbdb16dad1fd80f54ea77d7f6f7
pveran01 <148263135+pveran01@users.noreply.github.com>
2023-11-02 13:22:34 -0400

    Code for adding pages

--- components/MobileApp.tsx
+++ components/MobileApp.tsx
@@ -1,4 +1,5 @@
 import { Double, ObjectId } from "mongodb";
+import Link from "next/link";
 
 export interface MobileApp {
     id: string;
@@ -23,7 +24,9 @@ const MApp: React.FC<MAppProps> = ({app}) => {
         <div className="overflow-hidden shadow-lg rounded-lg">
           <img src={app.image} className="w-full rounded-lg"/>
           <div className="px-6 py-4">
-            <p className="font-bold text-xl mb-0 underline">{app.name}</p>
+            <Link href={`/app/${app.id}`}>
+                <p className="font-bold text-xl mb-0 underline">{app.name}</p>
+            </Link>
             <p className="font-bold text-m mb-2">{app.developer}</p>
             <p className="text-gray-700 text-base">{app.description}</p>
             <p className="font-bold text-yellow-500">{app.rating}</p>
---
+++ pages/app/[id].tsx
@@ -0,0 +1,66 @@
+import { useRouter } from "next/router";
+import { ObjectId } from 'mongodb';
+import clientPromise from "../../lib/mongodb";
+import { MobileApp } from "../../components/MobileApp";
+import { GetStaticProps, GetStaticPaths } from "next";
+
+function AppDetailsPage({ app }: {app: MobileApp }) {
+    const router = useRouter()
+    
+    if (router.isFallback) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            <h1>{app.name} Details</h1>
+        </div>
+    );
+}
+
+export async function getStaticPaths() {
+    const client = await clientPromise;
+    const db = client.db("NexuStore");
+
+    const apps = await db.collection("name").find( {} ).toArray();
+
+    const paths = apps.map((app) => ({ params: { id : app._id.toString() }}));
+
+    return {
+        paths,
+        fallback: true,
+    };
+}
+
+type Props = {
+    app: MobileApp;
+}
+
+export const getStaticProps: GetStaticProps<Props> = async( { params }) => {
+    if(!params?.id) {
+        return {
+            notFound: true,
+        };
+    }
+
+    const id = params.id as string;
+
+    const client = await clientPromise;
+    const db = client.db("NexuStore");
+
+    const app = await db.collection("name").find( {name: {$regex: /test/i}} );
+
+    if(!app) {
+        return {
+            notFound: true,
+        };
+    }
+
+    return {
+        props: {
+            app: JSON.parse(JSON.stringify(app)),
+        },
+    };
+}
+
+export default AppDetailsPage
